23b7a9a4cf8b6c10484c2946cfb7d3e8a85c3b52
David Toomer <djtoomer22@gmail.com>
2022-11-06 22:41:37 -0800

    feat: tests for deserializing cass files into cass trees

---
+++ tests/cass/test_cass.py
@@ -0,0 +1,18 @@
+import os
+
+import pytest
+
+from graph_code_embedding.cass import CassTree, load_file
+
+def test_deserialize_file_one_cass_tree() -> None:
+    file_name = os.path.join(os.path.dirname(__file__), 'examples', 'one_cass_tree.cas')
+    cass_trees = load_file(file_name)
+    assert(len(cass_trees) == 1)
+    assert(type(cass_trees[0]) == CassTree)
+
+def test_deserialize_file_multiple_cass_trees() -> None:
+    file_name = os.path.join(os.path.dirname(__file__), 'examples', 'multiple_cass_trees.cas')
+    cass_trees = load_file(file_name)
+    assert(len(cass_trees) == 4)
+    for cass_tree in cass_trees:
+        assert(type(cass_tree) == CassTree)
