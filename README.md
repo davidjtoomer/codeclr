5e15eeeb2a064afcd8ba4113456744ab787c2b58
David Toomer <djtoomer22@gmail.com>
2022-11-08 13:24:33 -0800

    edit: minor formatting change

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Final project for Stanford's CS329M (Machine Programming).
 The data for this project comes from [Project CodeNet](https://developer.ibm.com/exchanges/data/all/project-codenet/). We provide a Python script <code>download_data.py</code> for downloading and expanding all of the data into the appropriate directories. To download all of the data used in this project, you can run:
 
 ```
-python download_data.py --data_dir data --benchmark 1000 1400 --datatype code spts cass
+python download_data.py --data_dir data --benchmark 1000 1400 --data_type code spts cass
 ```
--- download_data.py
+++ download_data.py
@@ -12,9 +12,9 @@ logging.basicConfig(format='[%(asctime)s] %(pathname)s:%(lineno)d %(levelname)s 
 logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', type=str, default='data', help='The directory in which to download the data.')
 parser.add_argument('--benchmark', type=int, default=1000, nargs='+', choices=[1000, 1400], help='The benchmark number.')
-parser.add_argument('--datatype', type=str, default='code', nargs='+', choices=['code', 'spts', 'cass'], help='The type of data to download. SPT = simplified parse tree, CASS = context-aware semantics structure.')
+parser.add_argument('--data_dir', type=str, default='data', help='The directory in which to download the data.')
+parser.add_argument('--data_type', type=str, default='code', nargs='+', choices=['code', 'spts', 'cass'], help='The type of data to download. SPT = simplified parse tree, CASS = context-aware semantics structure.')
 args = parser.parse_args()
 
 # Create or clear the data directory
@@ -25,10 +25,10 @@ os.makedirs(args.data_dir)
 
 # Download tar archive to local disk
 DATA_ENDPOINT = f'https://dax-cdn.cdn.appdomain.cloud/dax-project-codenet/1.0.0/'
-get_filename = lambda benchmark, datatype: f'Project_CodeNet_C++{benchmark}{"" if datatype == "code" else "_" + datatype}.tar.gz'
+get_filename = lambda benchmark, data_type: f'Project_CodeNet_C++{benchmark}{"" if data_type == "code" else "_" + data_type}.tar.gz'
 for benchmark in args.benchmark:
-    for datatype in args.datatype:
-        file_name = get_filename(benchmark, datatype)
+    for data_type in args.data_type:
+        file_name = get_filename(benchmark, data_type)
         url = DATA_ENDPOINT + file_name
         file_path = os.path.join(args.data_dir, file_name)
 
