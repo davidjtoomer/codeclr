27bec88597f88e308013f1c3a4ea9ee420ad9bed
David Toomer <djtoomer22@gmail.com>
2022-12-04 13:09:14 -0800

    feat: add subtree masking to pretrain script

--- pretrain.sh
+++ pretrain.sh
@@ -2,11 +2,19 @@
 python pretrain.py --augment_1 node_drop --augment_2 node_drop &
 python pretrain.py --augment_1 node_drop --augment_2 node_mask &
 python pretrain.py --augment_1 node_drop --augment_2 identity &
+python pretrain.py --augment_1 node_drop --augment_2 subtree_mask &
 
 python pretrain.py --augment_1 node_mask --augment_2 node_drop &
 python pretrain.py --augment_1 node_mask --augment_2 node_mask &
 python pretrain.py --augment_1 node_mask --augment_2 identity &
+python pretrain.py --augment_1 node_mask --augment_2 subtree_mask &
 
 python pretrain.py --augment_1 identity --augment_2 node_drop &
 python pretrain.py --augment_1 identity --augment_2 node_mask &
 python pretrain.py --augment_1 identity --augment_2 identity &
+python pretrain.py --augment_1 identity --augment_2 subtree_mask &
+
+python pretrain.py --augment_1 subtree_mask --augment_2 node_drop &
+python pretrain.py --augment_1 subtree_mask --augment_2 node_mask &
+python pretrain.py --augment_1 subtree_mask --augment_2 identity &
+python pretrain.py --augment_1 subtree_mask --augment_2 subtree_mask &
